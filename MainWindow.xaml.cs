b73679938b65aa46897c8735fea56b8d87e61975
ST10061533 <105481917+ST10061533@users.noreply.github.com>
2024-06-24 13:51:11 +0200

    gui FILES

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticumGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //method to add the dash to labels
+            dashAdd();
+        }
+
+        private void dashAdd()
+        {
+            label.Content = "---------------------------------------------------------------------------------------------------------------------------";
+            label_Copy.Content = "---------------------------------------------------------------------------------------------------------------------------";
+        }
+    }
+}
